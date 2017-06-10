7c621a6ff2a2dcf8c7c84e91e0e8ebc8fb5a4657
Hack Reactor Student <student@hackreactor.com>
2017-06-09 17:12:00 -0700

    pass server GET request tests for testdata

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,62 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var url = require('url');
 // require more modules/folders here!
 
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10, // Seconds.
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  // use fs.readFile to read index.html
+  var statusCode;
+  var pathName = url.parse(req.url).pathname;
+
+  if (req.method === 'GET') {
+    if (req.url === '/') { // case that returns content of index.html
+      // gets data needed by client
+      fs.readFile(__dirname + '/public/index.html', 'utf-8', (err, content) => {
+        if (err) {
+          statusCode = 404;
+          res.writeHead(statusCode, defaultCorsHeaders);
+          res.end('Error 404: URL not found');
+        } else {
+          statusCode = 200;
+          res.writeHead(statusCode, defaultCorsHeaders);
+          res.end(content);
+        }
+      });
+    } else {
+      fs.readFile(__dirname + '/../test/testdata/sites' + pathName, 'utf-8', (err, content) => {
+        if (err) {
+          statusCode = 404;
+          res.writeHead(statusCode, defaultCorsHeaders);
+          res.end('Error 404: URL not found');
+        } else {
+          statusCode = 200;
+          res.writeHead(statusCode, defaultCorsHeaders);
+          res.end(content);
+        }
+      });
+    }
+  }
+  // var data;
+  
+  // fs.readFile(__dirname + '/public/index.html', 'utf-8', (err, content) => {
+  //   // add error case for when readfile fails
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     data = content;
+  //   }
+  // });
+  
+  // console.log(res);
+  // res.end(data);
+
+  // res.end(archive.paths.list);
 };
